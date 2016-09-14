448e109d5ae4edff2d2dcf40a97c28b99df3997a
hye <greenar@gmail.com>
2016-09-14 15:34:22 +1000

    section 2.2.1 pass

--- chapter2/ribcage.rkt
+++ chapter2/ribcage.rkt
@@ -6,38 +6,36 @@
 
 (define extend-env
   (lambda (var val env)
-    (cons (cons var val) env)))
+    (cons (cons (list var) (list (list val))) env)))
 
 (define extend-env*
   (lambda (vars vals env)
-    (cond
-      [(or (null? vars) (null? vals))  env]
-      [else
-       (extend-env*
-        (cdr vars)
-        (cdr vals)
-        (cons (cons (car vars) (car vals)) env))])))
+    (cons (cons vars (list vals)) env)))
 
 (define apply-env
   (lambda (env search-var)
     (cond
       [(null? (car env))
        (report-no-binding-found search-var)]
-      [(eqv? (caar env) search-var) (cdar env)]
+      [(eqv? (caaar env) search-var) (caadar env)]
+      [(null? (cdaar env))
+       (apply-env (cdr env) search-var)]
       [else
-       (apply-env (cdr env) search-var)])))
+       (apply-env
+        (cons (cons (cdaar env) (list (cdadar env))) (cdr env))
+        search-var)])))
 
 (define empty-env?
   (lambda (env)
     (null? env)))
 
-(define has-binding?
-  (lambda (s env)
-    (cond
-      [(null? env) #f]
-      [(eqv? (caar env) s) #t]
-      [else
-       (has-binding? s (cdr env))])))
+;; (define has-binding?
+;;   (lambda (s env)
+;;     (cond
+;;       [(null? env) #f]
+;;       [(eqv? (caar env) s) #t]
+;;       [else
+;;        (has-binding? s (cdr env))])))
 
 (define report-no-binding-found
   (lambda (search-var)
@@ -52,11 +50,11 @@
 (define e1
   (extend-env* '(a b c) '(1 2 3 4) (empty-env)))
 
-(equal?? (apply-env e 'd) 6)
-(equal?? (apply-env e 'y) 8)
-(equal?? (apply-env e 'x) 7)
-(equal?? (has-binding? 'z e) #f)
+;; (equal?? (apply-env e 'd) 6)
+;; (equal?? (apply-env e 'y) 8)
+;; (equal?? (apply-env e 'x) 7)
+;(equal?? (has-binding? 'z e) #f)
 
-(equal?? (apply-env e1 'a) 1)
+;(equal?? (apply-env e1 'a) 1)
 
 (report-unit-tests-completed 'apply-env)
