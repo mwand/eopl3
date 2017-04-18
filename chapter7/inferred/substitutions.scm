5f1842c610f3d338554b986a79ad25baa14031bc
wangwangwar <wangwangwar@gmail.com>
2017-04-18 22:30:56 +0800

    Ex7.18

--- chapter7/inferred/substitutions.scm
+++ chapter7/inferred/substitutions.scm
@@ -1,10 +1,18 @@
-(module substitutions  (lib "eopl.ss" "eopl")
+(module substitutions (lib "eopl.ss" "eopl")
+        
+        (require racket/base)
+        (require racket/mpair)
         (require "drscheme-init.scm")
         (require "lang.scm")
         (require "data-structures.scm")
         (require "../../utils.scm")
         
-        (provide substitution? empty-subst extend-subst apply-subst-to-type)
+        (provide substitution? 
+                 empty-subst 
+                 extend-subst
+                 extend-subst-ex7.18
+                 apply-subst-to-type
+                 apply-subst-to-type-ex7.18)
         
         ;;;;;;;;;;;;;;;; Unit substitution ;;;;;;;;;;;;;;;;
         
@@ -62,9 +70,9 @@
         
         ;; Ex7.17
         ;; Page: 262
+        ;;
         (define apply-subst-to-type
           (lambda (ty subst)
-            (display subst)
             (cases type ty
                    (int-type () (int-type))
                    (bool-type () (bool-type))
@@ -75,11 +83,38 @@
                    (tvar-type (sn)
                               (let ((tmp (assoc ty subst)))
                                 (if tmp
-                                    ;; t(σ[tv = t']) = (tσ)[tv = t']
-                                    (reduce (lambda (sub ty) 
-                                              (apply-one-subst ty (car sub) (cdr sub))) 
-                                            subst 
-                                            (cdr tmp))
+                                    ; t(σ[tv = t']) = (tσ)[tv = t']
+                                  (reduce (lambda (sub ty)
+                                            (apply-one-subst ty (car sub) (cdr sub)))
+                                          subst
+                                          (cdr tmp))
+                                  ty))))))
+        
+        ;; Page: 262
+        ;; Exercise 7.18 [**] Modify the implementation in the preceding exercise so that
+        ;; apply-subst-to-type computes the substitution for any type variable at most once.
+        ;;
+        ;; https://github.com/chenyukang/eopl/blob/master/ch7/18.scm
+        ;; Racket replace `cons`, `car`, `cdr` and so on to `mcons`, `mcar`, `mcdr` to indicate they are mutable version.
+        (define apply-subst-to-type-ex7.18
+          (lambda (ty subst)
+            (cases type ty
+                   (int-type () (int-type))
+                   (bool-type () (bool-type))
+                   (proc-type (t1 t2)
+                              (proc-type
+                               (apply-subst-to-type-ex7.18 t1 subst)
+                               (apply-subst-to-type-ex7.18 t2 subst)))
+                   (tvar-type (sn)
+                              (let ((tmp (massoc ty subst)))
+                                (if tmp
+                                    (if (type-var? (mcdr tmp))
+                                        (let ((real-type (apply-subst-to-type-ex7.18 (mcdr tmp) subst)))
+                                          (begin
+                                           (eopl:printf "*apply-subst-to-type* type-var: ~s, its real type: ~s\n" ty real-type)
+                                           (set-mcar! tmp real-type)
+                                           real-type))
+                                      (mcdr tmp))
                                   ty))))))
         
         ;; empty-subst : () -> Subst
@@ -131,5 +166,15 @@
         (define extend-subst
           (lambda (subst tvar ty)
             (cons (cons tvar ty) subst)))
+
+        ;; Ex7.18
+        (define extend-subst-ex7.18
+          (lambda (subst tvar ty)
+            (mcons (mcons tvar ty) subst)))
         
+        (define type-var?
+          (lambda (ty)
+            (cases type ty
+                   (tvar-type (_) #t)
+                   (else #f))))
         )
