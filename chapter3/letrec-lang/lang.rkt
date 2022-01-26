b8b421f8a953b5a44ce4fd8c3aaf33ee645a6b9e
hgye <greenar@gmail.com>
2022-01-26 14:35:16 +0800

    letrec-lang to racket

--- chapter3/letrec-lang/lang.rkt
+++ chapter3/letrec-lang/lang.rkt
@@ -1,72 +1,70 @@
-(module lang (lib "eopl.ss" "eopl")                
+#lang eopl
 
-  ;; grammar for the LETREC language
+;; grammar for the LETREC language
 
-  (require "drscheme-init.scm")
-  
-  (provide (all-defined-out))
+(require "drscheme-init.rkt")
 
-  ;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
-  
-  (define the-lexical-spec
-    '((whitespace (whitespace) skip)
-      (comment ("%" (arbno (not #\newline))) skip)
-      (identifier
-       (letter (arbno (or letter digit "_" "-" "?")))
-       symbol)
-      (number (digit (arbno digit)) number)
-      (number ("-" digit (arbno digit)) number)
-      ))
-  
-  (define the-grammar
-    '((program (expression) a-program)
+(provide (all-defined-out))
 
-      (expression (number) const-exp)
-      (expression
-        ("-" "(" expression "," expression ")")
-        diff-exp)
-      
-      (expression
-       ("zero?" "(" expression ")")
-       zero?-exp)
+;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
 
-      (expression
-       ("if" expression "then" expression "else" expression)
-       if-exp)
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
 
-      (expression (identifier) var-exp)
+(define the-grammar
+  '((program (expression) a-program)
 
-      (expression
-       ("let" identifier "=" expression "in" expression)
-       let-exp)   
+    (expression (number) const-exp)
+    (expression
+     ("-" "(" expression "," expression ")")
+     diff-exp)
 
-      (expression
-       ("proc" "(" identifier ")" expression)
-       proc-exp)
+    (expression
+     ("zero?" "(" expression ")")
+     zero?-exp)
 
-      (expression
-       ("(" expression expression ")")
-       call-exp)
+    (expression
+     ("if" expression "then" expression "else" expression)
+     if-exp)
 
-      (expression
-        ("letrec"
-          identifier "(" identifier ")" "=" expression
-           "in" expression)
-        letrec-exp)
-      
-      ))
-  
-  ;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
-  
-  (sllgen:make-define-datatypes the-lexical-spec the-grammar)
-  
-  (define show-the-datatypes
-    (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
-  
-  (define scan&parse
-    (sllgen:make-string-parser the-lexical-spec the-grammar))
-  
-  (define just-scan
-    (sllgen:make-string-scanner the-lexical-spec the-grammar))
-  
-  )
+    (expression (identifier) var-exp)
+
+    (expression
+     ("let" identifier "=" expression "in" expression)
+     let-exp)
+
+    (expression
+     ("proc" "(" identifier ")" expression)
+     proc-exp)
+
+    (expression
+     ("(" expression expression ")")
+     call-exp)
+
+    (expression
+     ("letrec"
+      identifier "(" identifier ")" "=" expression
+      "in" expression)
+     letrec-exp)
+
+    ))
+
+;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+
+(sllgen:make-define-datatypes the-lexical-spec the-grammar)
+
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
+(define scan&parse
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+(define just-scan
+  (sllgen:make-string-scanner the-lexical-spec the-grammar))
--- chapter3/letrec-lang/top.rkt
+++ chapter3/letrec-lang/top.rkt
@@ -1,70 +1,63 @@
-(module top (lib "eopl.ss" "eopl")
-  
-  ;; top level module.  Loads all required pieces.
-  ;; Run the test suite with (run-all).
+#lang eopl
 
-  (require "drscheme-init.scm")
-  (require "data-structures.scm")  ; for expval constructors
-  (require "lang.scm")             ; for scan&parse
-  (require "interp.scm")           ; for value-of-program
-  (require "tests.scm")            ; for test-list
-  
-  ;; since this is the top-level module, we don't really need to
-  ;; provide anything, but we do so just in case.  
+;; top level module.  Loads all required pieces.
+;; Run the test suite with (run-all).
+(require "drscheme-init.rkt")
+(require "data-structures.rkt")  ; for expval constructors
+(require "lang.rkt")             ; for scan&parse
+(require "interp.rkt")           ; for value-of-program
+(require "tests.rkt")            ; for test-list
 
-  (provide run run-all)
-  
-  ;;; interface for book test ;;;
-  (provide test-all)
-  (define (test-all) (run-all))
+;; since this is the top-level module, we don't really need to
+;; provide anything, but we do so just in case.
 
-  ;;;;;;;;;;;;;;;; interface to test harness ;;;;;;;;;;;;;;;;
-  
-  ;; run : String -> ExpVal
-  (define run
-    (lambda (string)
-      (value-of-program (scan&parse string))))
-  
-  ;; run-all : () -> Unspecified
+(provide run run-all)
 
-  ;; runs all the tests in test-list, comparing the results with
-  ;; equal-answer?  
+;;; interface for book test ;;;
+(provide test-all)
+(define (test-all) (run-all))
 
-  (define run-all
-    (lambda ()
-      (run-tests! run equal-answer? test-list)))
-  
-  (define equal-answer?
-    (lambda (ans correct-ans)
-      (equal? ans (sloppy->expval correct-ans))))
-  
-  (define sloppy->expval 
-    (lambda (sloppy-val)
+;;;;;;;;;;;;;;;; interface to test harness ;;;;;;;;;;;;;;;;
+
+;; run : String -> ExpVal
+(define run
+  (lambda (string)
+    (value-of-program (scan&parse string))))
+
+;; run-all : () -> Unspecified
+
+;; runs all the tests in test-list, comparing the results with
+;; equal-answer?
+
+(define run-all
+  (lambda ()
+    (run-tests! run equal-answer? test-list)))
+
+(define equal-answer?
+  (lambda (ans correct-ans)
+    (equal? ans (sloppy->expval correct-ans))))
+
+(define sloppy->expval
+  (lambda (sloppy-val)
+    (cond
+      ((number? sloppy-val) (num-val sloppy-val))
+      ((boolean? sloppy-val) (bool-val sloppy-val))
+      (else
+       (eopl:error 'sloppy->expval
+                   "Can't convert sloppy value to expval: ~s"
+                   sloppy-val)))))
+
+;; run-one : Sym -> ExpVal
+
+;; (run-one sym) runs the test whose name is sym
+
+(define run-one
+  (lambda (test-name)
+    (let ((the-test (assoc test-name test-list)))
       (cond
-        ((number? sloppy-val) (num-val sloppy-val))
-        ((boolean? sloppy-val) (bool-val sloppy-val))
-        (else
-         (eopl:error 'sloppy->expval 
-                     "Can't convert sloppy value to expval: ~s"
-                     sloppy-val)))))
-    
-  ;; run-one : Sym -> ExpVal
+        ((assoc test-name test-list)
+         => (lambda (test)
+              (run (cadr test))))
+        (else (eopl:error 'run-one "no such test: ~s" test-name))))))
 
-  ;; (run-one sym) runs the test whose name is sym
-  
-  (define run-one
-    (lambda (test-name)
-      (let ((the-test (assoc test-name test-list)))
-        (cond
-          ((assoc test-name test-list)
-           => (lambda (test)
-                (run (cadr test))))
-          (else (eopl:error 'run-one "no such test: ~s" test-name))))))
- 
-  ;; (run-all)
-  
-  )
-
-
-
-
+;; (run-all)
