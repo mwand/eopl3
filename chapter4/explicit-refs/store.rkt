f16c133bc65f24ef7f146e177ad92cfdb5b89156
hgye <greenar@gmail.com>
2022-03-08 14:26:41 +0800

    explicit-refs list->vector,  ex4.9

--- chapter4/explicit-refs/store.rkt
+++ chapter4/explicit-refs/store.rkt
@@ -1,5 +1,8 @@
 #lang eopl
 
+(require (only-in racket
+                  vector-append))
+
 (require "drscheme-init.rkt")
 
 (provide initialize-store! reference? newref deref setref!
@@ -19,7 +22,8 @@
 ;; empty-store : () -> Sto
 ;; Page: 111
 (define empty-store
-  (lambda () '()))
+  (lambda () (make-vector 0)))
+  ;; (lambda () '()))
 
 ;; initialize-store! : () -> Sto
 ;; usage: (initialize-store!) sets the-store to the empty-store
@@ -44,9 +48,9 @@
 ;; Page: 111
 (define newref
   (lambda (val)
-    (let ((next-ref (length the-store)))
+    (let ((next-ref (vector-length the-store)))
       (set! the-store
-            (append the-store (list val)))
+            (vector-append the-store (vector val)))
       (when (instrument-newref)
         (eopl:printf
          "newref: allocating location ~s with initial contents ~s~%"
@@ -57,29 +61,33 @@
 ;; Page 111
 (define deref
   (lambda (ref)
-    (list-ref the-store ref)))
+    (vector-ref the-store ref)))
 
 ;; setref! : Ref * ExpVal -> Unspecified
 ;; Page: 112
 (define setref!
   (lambda (ref val)
-    (set! the-store
-          (letrec
-              ((setref-inner
-                ;; returns a list like store1, except that position ref1
-                ;; contains val.
-                (lambda (store1 ref1)
-                  (cond
-                    ((null? store1)
-                     (report-invalid-reference ref the-store))
-                    ((zero? ref1)
-                     (cons val (cdr store1)))
-                    (else
-                     (cons
-                      (car store1)
-                      (setref-inner
-                       (cdr store1) (- ref1 1))))))))
-            (setref-inner the-store ref)))))
+    (begin (display ref)
+           (newline))
+    (cond [(> ref (vector-length the-store)) (report-invalid-reference ref the-store)]
+          [else (vector-set! the-store ref val)])))
+    ;; (set! the-store
+    ;;       (letrec
+    ;;           ((setref-inner
+    ;;             ;; returns a list like store1, except that position ref1
+    ;;             ;; contains val.
+    ;;             (lambda (store1 ref1)
+    ;;               (cond
+    ;;                 ((null? store1)
+    ;;                  (report-invalid-reference ref the-store))
+    ;;                 ((zero? ref1)
+    ;;                  (cons val (cdr store1)))
+    ;;                 (else
+    ;;                  (cons
+    ;;                   (car store1)
+    ;;                   (setref-inner
+    ;;                    (cdr store1) (- ref1 1))))))))
+    ;;         (setref-inner the-store ref)))))
 
 (define report-invalid-reference
   (lambda (ref the-store)
@@ -96,13 +104,14 @@
 ;; Replaces get-store (p. 111)
 (define get-store-as-list
   (lambda ()
-    (letrec
-        ((inner-loop
-          ;; convert sto to list as if its car was location n
-          (lambda (sto n)
-            (if (null? sto)
-                '()
-                (cons
-                 (list n (car sto))
-                 (inner-loop (cdr sto) (+ n 1)))))))
-      (inner-loop the-store 0))))
+    (vector->list the-store)))
+    ;; (letrec
+    ;;     ((inner-loop
+    ;;       ;; convert sto to list as if its car was location n
+    ;;       (lambda (sto n)
+    ;;         (if (null? sto)
+    ;;             '()
+    ;;             (cons
+    ;;              (list n (car sto))
+    ;;              (inner-loop (cdr sto) (+ n 1)))))))
+    ;;   (inner-loop the-store 0))))
