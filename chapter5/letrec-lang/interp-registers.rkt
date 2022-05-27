fab03fc25ceebe0f05950318ae51505afe1fba52
hgye <greenar@gmail.com>
2022-05-27 23:11:40 +0800

    multi-args for registerization interpreter, ex5.25

--- chapter5/letrec-lang/interp-registers.rkt
+++ chapter5/letrec-lang/interp-registers.rkt
@@ -8,6 +8,7 @@
   (require "lang.rkt")
   (require "data-structures.rkt")
   (require "environments.rkt")
+  (require "store.rkt")
 
   (provide value-of-program value-of/k)
 
@@ -29,6 +30,7 @@
   ;; Page: 167
   (define value-of-program
     (lambda (pgm)
+      (initialize-store!)
       (cases program pgm
         (a-program (body)
           (set! cont (end-cont))
@@ -56,7 +58,7 @@
           (apply-cont))
         (var-exp (var)
           ;; (apply-cont cont (apply-env env id)))
-          (set! val (apply-env env var))
+          (set! val (deref (apply-env env var)))
           ;; cont is unchanged
           (apply-cont))
         (proc-exp (var body)
@@ -69,18 +71,24 @@
           ;;   cont)
           (set! exp letrec-body)
           (set! env
-            (extend-env-rec p-name b-var p-body env))
+            (extend-env-rec* p-name b-var p-body env))
           (value-of/k))
         (zero?-exp (exp1)
           ;; (value-of/k exp1 env (zero1-cont cont))
           (set! cont (zero1-cont cont))
           (set! exp exp1)
           (value-of/k))
-        (let-exp (var exp1 body)
+        (let-exp (vars exps body)
           ;; (value-of/k rhs env (let-exp-cont id body env cont))
-          (set! cont (let-exp-cont var body env cont))
-          (set! exp exp1)
-          (value-of/k))
+                 (if (null? exps)
+                     (value-of/k)
+                     (begin
+                       (set! cont (let-head-cont vars (cdr exps) body env cont))
+                       (set! exp (car exps))
+                       (value-of/k))))
+          ;; (set! cont (let-exp-cont var body env cont))
+          ;; (set! exp exps)
+          ;; (value-of/k))
         (if-exp (exp1 exp2 exp3)
           ;; (value-of/k exp0 env (if-test-cont exp2 exp3 env cont))
           (set! cont (if-test-cont exp2 exp3 env cont))
@@ -92,11 +100,13 @@
           (set! exp exp1)
           ;; env is unchanged
           (value-of/k))
-        (call-exp (rator rand)
+        (call-exp (rator rands)
           ;; (value-of/k rator env (rator-cont rand env cont))
-          (set! cont (rator-cont rand env cont))
+          (set! cont (rator-cont rands env cont))
           (set! exp rator)
           (value-of/k))
+        (else
+         (eopl:error 'vaule-of/k "not implements"))
         )))
 
   ;; apply-cont : Cont * ExpVal -> FinalAnswer
@@ -123,8 +133,20 @@
           ;; (value-of/k body (extend-env id val env) cont)
           (set! cont saved-cont)
           (set! exp body)
-          (set! env (extend-env var val saved-env))
+          (set! env (extend-env var (newref val) saved-env))
           (value-of/k))
+        (let-head-cont (vars exps body saved-env saved-cont)
+                       (if (null? exps)
+                           (begin
+                             (set! exp body)
+                             (set! env (extend-env (car vars) (newref val) saved-env))
+                             (set! cont saved-cont)
+                             (value-of/k))
+                           (begin
+                             (set! exp (let-exp (cdr vars) exps body))
+                             (set! env (extend-env (car vars) (newref val) saved-env))
+                             (set! cont saved-cont)
+                             (value-of/k))))
         (if-test-cont (exp2 exp3 saved-env saved-cont)
           (set! cont saved-cont)
           (if (expval->bool val)
@@ -145,19 +167,44 @@
             (set! cont saved-cont)
             (set! val (num-val (- num1 num2)))
             (apply-cont)))
-        (rator-cont (rand saved-env saved-cont)
+        (rator-cont (rands saved-env saved-cont)
+                    (if (null? rands)
+                        (begin
+                          (set! cont saved-cont)
+                          (set! proc1 (expval->proc val))
+                          (set! val (list))
+                          (apply-procedure/k))
+                        (begin
+                          (set! cont (rands-cont val (list) (cdr rands) saved-env saved-cont))
+                          (set! exp (car rands))
+                          (set! env saved-env)
+                          (value-of/k))))
           ;; (value-of/k rand env (rand-cont val cont))
-          (set! cont (rand-cont val saved-cont))
-          (set! exp rand)
-          (set! env saved-env)
-          (value-of/k))
-        (rand-cont (rator-val saved-cont)
+          ;; (set! cont (rands-cont val val val saved-env saved-cont))
+          ;; (set! exp rand)
+          ;; (set! env saved-env)
+          ;; (value-of/k))
+        (rands-cont (rator-val val2 tail-rands saved-env saved-cont)
           (let ((rator-proc (expval->proc rator-val)))
+            (if (null? tail-rands)
+                (begin
+                  (set! cont saved-cont)
+                  (set! proc1 rator-proc)
+                  (set! val (append val2 (list val)))
+                  (apply-procedure/k))
+                (begin
+                  (set! cont (rands-cont rator-val (append val2 (list val))
+                                         (cdr tail-rands) saved-env saved-cont))
+                  (set! env saved-env)
+                  (set! exp (car tail-rands))
+                  (value-of/k)))))
             ;; (apply-procedure rator-proc rator-val cont)
-            (set! cont saved-cont)
-            (set! proc1 rator-proc)
-            (set! val val)
-            (apply-procedure/k)))
+            ;; (set! cont saved-cont)
+            ;; (set! proc1 rator-proc)
+            ;; (set! val val)
+            ;; (apply-procedure/k)))
+        (else
+         (eopl:error 'apply-cont "not implements"))
         )))
 
   ;; apply-procedure : Proc * ExpVal -> ExpVal
@@ -170,9 +217,9 @@
   (define apply-procedure/k
     (lambda ()
       (cases proc proc1
-        (procedure (var body saved-env)
+        (procedure (vars body saved-env)
           (set! exp body)
-          (set! env (extend-env var val saved-env))
+          (set! env (extend-env* vars (map newref val) saved-env))
           (value-of/k)))))
 
   ;; instrumented version
